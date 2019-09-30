415ddcf10c22711b929d97d9328e4f8caf1672f8
= <=>
2019-09-30 11:09:18 -0500

    able to jankily import functions from _utils.py

---
+++ tests/unit_tests/test_utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import openmc
+import pytest
+# 
+# import sys
+# import os
+
+import _utils
+
+print('hello')
+
+
+# openmc.Download('git-scm.com/book/en/v2/Git-Branching-Branches-in-a-Nutshell')
+
+print('world')
