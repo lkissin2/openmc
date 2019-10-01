4a87c2175b98aa962008137040a5518cf01b7810
= <=>
2019-10-01 15:19:36 -0500

    made as_browser True

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -7,13 +7,14 @@ import os
 
 @pytest.fixture(scope='module')
 def download_photos():
-    """use _utils download to download the same picture from two different urls
-       so the files get different names and can be compared to each other and
-       repeat a third time to test the 'already downloaded' lines"""
+    """use _utils download() function to download the same picture three times,
+       twice to get unique names, and a third time to use the already downloaded
+       block of code"""
     _utils.download('https://media.gettyimages.com/photos/' +
-                    'flamingo-picture-id957054810?s=2048x2048')
-    _utils.download('https://tinyurl.com/y5cmtka3')
-    _utils.download('https://tinyurl.com/y5cmtka3')
+                    'flamingo-picture-id957054810?s=2048x2048',
+                     as_browser = True)
+    _utils.download('https://tinyurl.com/y5cmtka3', as_browser = True)
+    _utils.download('https://tinyurl.com/y5cmtka3', as_browser = True)
 
 
 def test_photos(download_photos):
