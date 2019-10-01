ad5c73b2f70407c17df5536dec5076bd31e5f04c
= <=>
2019-10-01 13:08:45 -0500

    run the test with a pytest fixture so that it actually works

--- tests/unit_tests/test_utils.py
+++ tests/unit_tests/test_utils.py
@@ -4,11 +4,17 @@ import pytest
 
 import os
 
-print('hello')
-_utils.download('https://media.gettyimages.com/photos/flamingo-picture-id957054810?s=2048x2048')
-_utils.download('https://tinyurl.com/y5cmtka3')
-assert os.path.getsize('y5cmtka3') == os.path.getsize('flamingo-picture-id957054810')
-print('world')
-os.remove('y5cmtka3')
-os.remove('flamingo-picture-id957054810')
-print('!')
+
+@pytest.fixture(scope='module')
+def download_photos():
+    print('hello')
+    _utils.download('https://media.gettyimages.com/photos/flamingo-picture-id957054810?s=2048x2048')
+    _utils.download('https://tinyurl.com/y5cmtka3')
+    print('world')
+    print('!')
+
+
+def test_photos(download_photos):
+    assert os.path.getsize('y5cmtka3') == os.path.getsize('flamingo-picture-id957054810')
+    os.remove('y5cmtka3')
+    os.remove('flamingo-picture-id957054810')
